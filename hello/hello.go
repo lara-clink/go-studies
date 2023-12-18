92336793a2ec075c57184c82e6ab5f5cea2f859a
lara.clink <lara.clink@quintoandar.com.br>
2023-12-18 16:49:42 -0300

    add monitoring function and for loop

--- hello/hello.go
+++ hello/hello.go
@@ -2,36 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando... ")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando")
+			os.Exit(-1)
+		}
 	}
 }
 
 func exibeIntroducao() {
 	nome := "Lara"
-	versao := 1.1
+	versao := 1.2
 	fmt.Println("Olá, sr(a).", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
 
 func exibeMenu() {
+	fmt.Println("Menu:")
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
@@ -43,4 +47,16 @@ func leComando() int {
 	fmt.Println("O valor da variável comando é:", comandoLido)
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando... ")
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+	//fmt.Println(resp)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
